5e3c15087a41c1daaf7bf7e02bb27d5acd5eed01
kentnf <kentnf@gmail.com>
2015-02-22 06:12:13 +0000

    update install

--- Install.sh
+++ Install.sh
@@ -5,5 +5,5 @@ gunzip databases/vrl_plant.gz
 ./bin/samtools faidx databases/vrl_plant
 ./bin/bwa index databases/vrl_plant
 ./bin/formatdb -i databases/vrl_plant -p F
+./bin/formatdb -i databases/vrl_plant_prot -p T
 
-gunzip test_data.gz
